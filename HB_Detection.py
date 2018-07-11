6ab2b64199c28ed37f7cea0c9a4a4f994ae65e1e
Anderson Castilhos <castilhos_cmpa@hotmail.com>
2018-07-11 19:48:18 -0300

    Teste de Elapsed Time

--- HB_Detection.py
+++ HB_Detection.py
@@ -1,4 +1,14 @@
 
-
+#classe mãe de cada uma das etapas, contém os métodos comuns
 class HB_Detection(object):
+    
+    KB_KEY = 10
+    
+    hb_pressed_times = []
+    
+    def write_detection(self,elapsed_time):
+        
+        hb_pressed_times.append(elapsed_time)
+                
+    
     
--- Main.py
+++ Main.py
@@ -1,10 +1,26 @@
-from CondicaoMotoraS1 import CondicaoMotoraS1
-from PreInteroceptivaS2 import PreInteroceptivaS2
-from FeedbackS3 import FeedbackS3
-from PosInteroceptivaS4 import PosInteroceptivaS4
+#from CondicaoMotoraS1 import CondicaoMotoraS1
+#from PreInteroceptivaS2 import PreInteroceptivaS2
+#from FeedbackS3 import FeedbackS3
+#from PosInteroceptivaS4 import PosInteroceptivaS4
+
+import time
 
 
 class Main(object):
+    
+    teste = 0
+    
+    start_time = time.time()
+    
     print("ola")
+    for i in range(0,10000000):
+        teste+=1
+    print("mundo")
+    elapsed_time = time.time() - start_time
+    
+    print(start_time)
+    print(elapsed_time)
+    
+   
 
     
